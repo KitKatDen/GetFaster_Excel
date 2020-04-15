9098229477998148f9716c167e92338daa240877
KitKatDen <maldenser@yandex.ru>
2020-04-15 12:35:23 +0300

    GetFaster IQOS Excel Bitrix24-to-QuickRun v2.0 *добавлен поиск по таблице столбца по заголовку которого необходимо сделать замену значений (вместо предопределённых столбцов B,C и т.д.)

--- batya.py
+++
@@ -1,3 +0,0 @@
-x = 10
-for i in range (2,x+1):
- print(i)
--- html_to_xlsx.py
+++ html_to_xlsx.py
@@ -5,11 +5,18 @@ table = pd.read_html('sale_order.xls')
 table[0].to_excel('data.xlsx', index=False, header=False)
 excelFile = openpyxl.load_workbook('data.xlsx')
 sheet1 = excelFile.active
+col_letter = ['No Letter','A','B','C','D','E','F','G','H','I','J','K',"L"]
 
-for row in range (2, sheet1.max_row+1):
- sheet1['B' + str(row)] = sheet1['B' + str(row)].value.replace('№', '')
- sheet1['C' + str(row)] = sheet1['C' + str(row)].value.replace('.00 руб.', '')
- date_order = sheet1['F' + str(row)].value.split().pop().split('-')
- sheet1['F' + str(row)] = date_order[0]
- sheet1['G' + str(row)] = date_order[1]
+for col in range (1,sheet1.max_column+1):
+ if sheet1[col_letter[col]+ '1'].value == 'ID':
+  for row in range(2, sheet1.max_row + 1):
+   sheet1[col_letter[col] + str(row)] = sheet1['B' + str(row)].value.replace('№', '')
+ if sheet1[col_letter[col]+ '1'].value == 'Сумма':
+  for row in range(2, sheet1.max_row + 1):
+   sheet1['C' + str(row)] = sheet1['C' + str(row)].value.replace('.00 руб.', '')
+ if sheet1[col_letter[col]+ '1'].value == 'Дата и время доставки':
+  for row in range(2, sheet1.max_row + 1):
+   date_order = sheet1[col_letter[col] + str(row)].value.split().pop().split('-')
+   sheet1[col_letter[col] + str(row)] = date_order[0]
+   sheet1[col_letter[col+1] + str(row)] = date_order[1]
 excelFile.save('data.xlsx')
