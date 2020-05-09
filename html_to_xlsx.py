0f936e6e1d691d0986492ae4f8bb98c8dee4da22
KitKatDen <maldenser@yandex.ru>
2020-05-09 21:21:27 +0300

    GetFaster IQOS Excel Bitrix24-to-QuickRun v4.0 *действия перенесены в 2 функции

--- html_to_xlsx.py
+++ html_to_xlsx.py
@@ -1,22 +1,37 @@
 import pandas as pd
 import openpyxl
 
-table = pd.read_html('sale_order.xls')
-table[0].to_excel('data.xlsx', index=False, header=False)
-excelFile = openpyxl.load_workbook('data.xlsx')
+table = pd.read_html("sale_order.xls")
+table[0].to_excel("data_refresh.xlsx", index=False, header=False)
+excelFile = openpyxl.load_workbook("data_refresh.xlsx")
 sheet1 = excelFile.active
-col_letter = ['No Letter','A','B','C','D','E','F','G','H','I','J','K','L']
+col_letter = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L")
 
-for col in range (1,sheet1.max_column+1):
- if sheet1[col_letter[col]+ '1'].value == 'ID':
-  for row in range(2, sheet1.max_row + 1):
-   sheet1[col_letter[col] + str(row)] = sheet1['B' + str(row)].value.replace('№', '')
- if sheet1[col_letter[col]+ '1'].value == 'Сумма':
-  for row in range(2, sheet1.max_row + 1):
-   sheet1['C' + str(row)] = sheet1['C' + str(row)].value.replace('.00 руб.', '')
- if sheet1[col_letter[col]+ '1'].value == 'Дата и время доставки':
-  for row in range(2, sheet1.max_row + 1):
-   date_order = sheet1[col_letter[col] + str(row)].value.split().pop().split('-')
-   sheet1[col_letter[col] + str(row)] = date_order[0]
-   sheet1[col_letter[col+1] + str(row)] = date_order[1]
-excelFile.save('data.xlsx')
+
+# поиск по верхней строчки столбца по содержанию текста, после замена в этом столбце одного символа на другой
+
+
+def s_and_r_in_col(col_name, what_change_to, what_change_for):
+    if sheet1[col_letter[col] + "1"].value == col_name:
+        for row in range(2, sheet1.max_row + 1):
+            sheet1[col_letter[col] + str(row)] = sheet1[col_letter[col] + str(row)].value.replace(what_change_to,
+                                                                                                  what_change_for)
+
+
+# поиск по верхней строчки столбца по содержанию текста, после разделение на два блока информации из ячейки на эту
+# ячейку и ячейку справа
+
+
+def s_and_split_in_2_cols(col_name_0, split_symbol):
+    if sheet1[col_letter[col] + "1"].value == col_name_0:
+        for row in range(2, sheet1.max_row + 1):
+            date_order = sheet1[col_letter[col] + str(row)].value.split().pop().split(split_symbol)
+            sheet1[col_letter[col] + str(row)] = date_order[0]
+            sheet1[col_letter[col + 1] + str(row)] = date_order[1]
+
+
+for col in range(0, sheet1.max_column + 1):
+    s_and_r_in_col("ID", "№", "")
+    s_and_r_in_col("Сумма", ".00 руб.", "")
+    s_and_split_in_2_cols("Дата и время доставки", "-")
+excelFile.save("data.xlsx")
