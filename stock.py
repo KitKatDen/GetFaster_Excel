b78b7ca73263510f2b4088d534b750e8d5a2b470
KitKatDen <maldenser@yandex.ru>
2020-06-15 12:52:54 +0300

    GetFaster IQOS Excel Bitrix24-to-QuickRun v4.0 *действия перенесены в 2 функции

--- stock.py
+++ stock.py
@@ -1,18 +1,13 @@
 import telebot
-import requests
 import wikipedia
-#import telepot
-from bs4 import BeautifulSoup
+from telebot import apihelper
+apihelper.proxy = {'http':'http://141.164.42.178:8080'}
 
-html = requests.get("https://ru.wikipedia.org/wiki/ИС-2")
 wikipedia.set_lang("ru")
 
-bot = telebot.TeleBot('1244076324:AAGx55Gq-HMB0y5wdI3uanCErd5BnnvGRYw');
+bot = telebot.TeleBot("1244076324:AAGx55Gq-HMB0y5wdI3uanCErd5BnnvGRYw");
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "test":
         bot.send_message(message.chat.id, wikipedia.summary("ИС-2"))
 bot.polling(none_stop=True, interval=0)
-
-message.from_chat.id
-print(bot.get_updates()[0].message.chat.id)
