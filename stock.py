e4ab0359f56241c4bbd36664260a8194ea96d466
KitKatDen <60482486+KitKatDen@users.noreply.github.com>
2020-08-11 20:46:14 +0300

    Update stock.py

--- stock.py
+++ stock.py
@@ -5,7 +5,7 @@ apihelper.proxy = {'http':'http://141.164.42.178:8080'}
 
 wikipedia.set_lang("ru")
 
-bot = telebot.TeleBot("1244076324:AAGx55Gq-HMB0y5wdI3uanCErd5BnnvGRYw");
+bot = telebot.TeleBot("token");
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "test":
